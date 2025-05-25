968b40f02fffcd60256ccd5b92416daa84bf32bb
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2025-05-25 21:12:55 +0800

    alvistack/0.5.3
        git clean -xdf
        tar zcvf ../python-sqlparse_0.5.3.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-sqlparse.spec ../python-sqlparse_0.5.3-1.spec
        cp ../python*-sqlparse*0.5.3*.{gz,xz,spec,dsc} /osc/home\:alvistack/andialbrecht-sqlparse-0.5.3/
        rm -rf ../python*-sqlparse*0.5.3*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='sqlparse',
+    version='0.5.3',
+    description='A non-validating SQL parser.',
+    author_email='Andi Albrecht <albrecht.andi@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Database',
+        'Topic :: Software Development',
+    ],
+    extras_require={
+        'dev': [
+            'build',
+            'hatch',
+        ],
+        'doc': [
+            'sphinx',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'sqlformat = sqlparse.__main__:main',
+        ],
+    },
+    packages=[
+        'sqlparse',
+        'sqlparse.engine',
+        'sqlparse.filters',
+    ],
+)
